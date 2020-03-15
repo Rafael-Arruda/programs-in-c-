a2fa096b8f19e6591f292ca734d666fefd37e4b6
Rafael-Arruda <62109053+Rafael-Arruda@users.noreply.github.com>
2020-03-15 12:29:03 -0300

    Create lanche.c

---
+++ lanche.c
@@ -0,0 +1,34 @@
+#include <stdio.h> 
+
+int main(){
+  
+  int codigo,qtd;
+  float total;
+  printf("Informe o código e a quantidade: \n");
+  scanf("%i",&codigo);
+  scanf("%i",&qtd);
+
+  switch(codigo){
+    case 1:
+      total = qtd*4;
+      printf("Total: R$ %.2f\n",total);
+      break;
+    case 2:
+      total = qtd*4.5;
+      printf("Total: R$ %.2f\n",total);
+      break;
+    case 3:
+      total = qtd*5;
+      printf("Total: R$ %.2f\n",total);
+      break;
+    case 4:
+      total = qtd*2;
+      printf("Total: R$ %.2f\n",total);
+      break;
+    case 5:
+      total = qtd*1.50;
+      printf("Total: R$ %.2f\n",total);
+      break;
+  }
+  return 0;
+}
