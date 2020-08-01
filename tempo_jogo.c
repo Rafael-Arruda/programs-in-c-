e7095f30872df2ab2ace48ee9f7f93c6829ebb73
Rafael-Arruda <62109053+Rafael-Arruda@users.noreply.github.com>
2020-08-01 13:56:24 -0300

    Create tempo_jogo.c

---
+++ tempo_jogo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void){
+    //URI 1047
+    int i;
+    int hi, mi, hf, mf;
+    scanf("%i %i %i %i", &hi, &mi, &hf, &mf);
+    int horas = 0;
+    if(hi==hf && mi > mf){
+        horas = 24;
+    }if(mi>mf){
+        horas -= 1;
+    }else if(hi==hf && mi==mf){
+        horas = 24;
+    }
+    int minutos = 0;
+    for(i=hi;i != hf;i++){
+        if(i == 24){
+            i = 0;
+        }
+        horas += 1;
+    }
+    for(i=mi;i != mf; i++){
+        if(i == 60){
+            i=0;
+        }
+        minutos++;
+    }
+    printf("O JOGO DUROU %i HORA(S) E %i MINUTO(S)\n", horas, minutos);
+}
